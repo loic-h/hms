02f5cf0f1cc6429d4e7ad7dd60cc3b35d8d73a54
Loïc Hamet <loic.hamet@gmail.com>
2021-02-21 19:57:07 +0100

    Play audio

--- src/components/track.vue
+++ src/components/track.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    {{ name }} – {{ artists.join(', ') }}
+    <span>
+      {{ name }} – {{ artists.join(', ') }}
+    </span>
+    <button
+      v-if="canplay"
+      @click="togglePlay">
+      {{ buttonCopy }}
+    </button>
   </div>
 </template>
 
@@ -12,6 +19,35 @@ export default {
     name: { type: String, default: null },
     artists: { type: Array, default: () => [] },
     preview: { type: String, default: null }
+  },
+  data() {
+    return {
+      audio: null,
+      canplay: false,
+      playing: false
+    };
+  },
+  computed: {
+    buttonCopy() {
+      return this.playing ? "Pause" : "Play";
+    }
+  },
+  mounted() {
+    console.log(this.preview)
+    this.audio = new Audio(this.preview);
+    this.audio.addEventListener('canplaythrough', () => {
+      this.canplay = true;
+    });
+  },
+  methods:{
+    togglePlay() {
+      if (this.playing) {
+        this.audio.pause();
+      } else {
+        this.audio.play();
+      }
+      this.playing = !this.playing;
+    }
   }
 };
 </script>
